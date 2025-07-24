9bd4093f28b56e969052c91c1fab17d29a05fce0
Козачук Александр <chibiz22@mail.ru>
2025-07-24 15:27:41 +0300

    optimize !

--- guess_number.py
+++ guess_number.py
@@ -2,16 +2,16 @@ from random import randint
 
 
 random_numberg = randint(1, 100)
-print('Угадайте число от 1 до 100')
+print('Угадайте число от 1 до 100!')
 
 def g():
     while True:
         if int(input()) < random_numberg:
-            print('Ваше число меньше того, что загадано')
+            print('Ваше число меньше того, что загадано!')
         elif int(input()) > random_numberg:
-            print('Ваше число больше того, что загадано')
+            print('Ваше число больше того, что загадано!')
         elif int(input()) == random_numberg:
-            print('Отличная интуиция! Вы угадали число')
+            print('Отличная интуиция! Вы угадали число!')
             break
         
 
